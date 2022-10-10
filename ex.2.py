ecde7607e545e25ae4bf4e43ae5c0b5636230ae8
FoggyUp <fabriciosm100@gmail.com>
2022-10-10 15:18:01 -0300

    Exercicio de input

---
+++ ex.2.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite a 1° nota: "))
+n2 = int(input('Digite a 2° nota:'))
+n3 = int(input('Digite a 3° nota:'))
+n4 = int(input('Digite a 4° nota:'))
+soma = n1 + n2 + n3 + n4
+media = soma / 4
+print("Media final",media)
