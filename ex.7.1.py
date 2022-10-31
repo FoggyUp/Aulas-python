f0d9af79d46a4f2ad34c9f31a22f6e69c7a216e0
FoggyUp <fabriciosm100@gmail.com>
2022-10-31 14:30:16 -0300

    Exercicio Python

---
+++ ex.7.1.py
@@ -0,0 +1,34 @@
+print('Digite seus documentos para votar!')
+
+rg = int(input('Digite o número do RG:'))
+if rg == 12345678:
+    print('RG Correto')
+    titulo = int(input('Digite o número do Título:'))
+    if titulo == 11122233344:
+        print('João do Carmo')
+        print('Pode Votar!')
+    else:
+        print('Eleitor não encontrado')
+        
+
+candidato = print('''
+CANDIDATO 1: Paulo Freire
+CANDIDATO 2: Jean Piaget''' )
+
+candidato1 = "Paulo Freire"
+candidato2 = "Jean Piaget"
+
+voto = int(input('Digite o número do seu Candidato a Presidencia:'))
+if voto == 20:
+    print('Candidato', candidato2)
+elif voto == 10:
+    print('Candidato', candidato1)
+
+
+    
+      
+
+
+
+
+ 
